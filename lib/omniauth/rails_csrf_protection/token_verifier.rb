16d5ec623050fe50b4928bc7a20e4c3838349afc
nick evans <nick@rubinick.dev>
2024-09-24 15:38:25 -0400

    Inherit config from ActionController::Base
    By using an inheritable_copy of the config (rather than just redefining
    the methods) we gain the ability to _override_ the config locally.

--- lib/omniauth/rails_csrf_protection/token_verifier.rb
+++ lib/omniauth/rails_csrf_protection/token_verifier.rb
@@ -13,19 +13,20 @@ module OmniAuth
     # authenticity token, you can find the source code at
     # https://github.com/rails/rails/blob/v5.2.2/actionpack/lib/action_controller/metal/request_forgery_protection.rb#L217-L240.
     class TokenVerifier
+      # Inherits config from ActionController::Base.
+      # Define _before_ including ActionController::RequestForgeryProtection.
+      def self.config
+        @_config ||= ActionController::Base.config.inheritable_copy
+      end
+
       include ActiveSupport::Configurable
       include ActionController::RequestForgeryProtection
 
       # `ActionController::RequestForgeryProtection` contains a few
       # configurable options. As we want to make sure that our configuration is
       # the same as what being set in `ActionController::Base`, we should make
-      # all out configuration methods to delegate to `ActionController::Base`.
-      config.each_key do |configuration_name|
-        undef_method configuration_name
-        define_method configuration_name do
-          ActionController::Base.config[configuration_name]
-        end
-      end
+      # our configuration delegate to `ActionController::Base`.
+      config.each_key do |key| config.delete(key) end
 
       def call(env)
         dup._call(env)
