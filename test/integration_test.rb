cookpad/omniauth-rails_csrf_protection

commit 2cf722bd7cc310009ee626cce28a7ea52b47f649

Add workaround for deprecated AS::Configurable
Given `ActiveSupport::Configurable` is deprecated without replacement
and will be removed in Rails 8.2, we can stop using it in Rails 8.1+
and instead create methods that delegate to
`ActionController::Base.config` instead.

Fixes #23, closes #24

Co-authored-by: Scott Klein <scott@klein.io>

lib/omniauth/rails_csrf_protection/token_verifier.rb
@@ -1,5 +1,9 @@
-require "active_support/configurable"
+require "action_pack/version"
 require "action_controller"
+
+unless ActionPack.version >= Gem::Version.new("8.1.a")
+  require "active_support/configurable"
+end
 
 module OmniAuth
   module RailsCsrfProtection
@@ -13,19 +17,35 @@ module OmniAuth
     # authenticity token, you can find the source code at
     # https://github.com/rails/rails/blob/v5.2.2/actionpack/lib/action_controller/metal/request_forgery_protection.rb#L217-L240.
     class TokenVerifier
-      include ActiveSupport::Configurable
-      include ActionController::RequestForgeryProtection
+      if ActionPack.version >= Gem::Version.new("8.1.a")
+        # `ActiveSupport::Configurable` is deprecated in Rails 8.1 and will be
+        # removed in Rails 8.2. As `ActionController::RequestForgeryProtection`
+        # directly accesing configurations via `config`, we only need to define
+        # these methods and delegate them to `ActionController::Base.config`.
+        def self.config
+          ActionController::Base.config
+        end
 
-      # `ActionController::RequestForgeryProtection` contains a few
-      # configurable options. As we want to make sure that our configuration is
-      # the same as what being set in `ActionController::Base`, we should make
-      # all out configuration methods to delegate to `ActionController::Base`.
-      config.each_key do |configuration_name|
-        undef_method configuration_name
-        define_method configuration_name do
-          ActionController::Base.config[configuration_name]
+        def config
+          self.class.config
+        end
+      else
+        include ActiveSupport::Configurable
+
+        # `ActionController::RequestForgeryProtection` contains a few
+        # configurable options. As we want to make sure that our configuration is
+        # the same as what being set in `ActionController::Base`, we should make
+        # all out configuration methods to delegate to `ActionController::Base`.
+        config.each_key do |configuration_name|
+          undef_method configuration_name if defined?(configuration_name)
+          define_method configuration_name do
+            ActionController::Base.config[configuration_name]
+          end
         end
       end
+
+      # Include this module only after we've prepared the configuration
+      include ActionController::RequestForgeryProtection
 
       def call(env)
         dup._call(env)

commit 1a0d39099957675f549e1f23c836f57b99b09a00

Fix an issue with configuration delegation in Rails < 8.1
A bug was introduced in 2cf722bd7cc310009ee626cce28a7ea52b47f649 that
caused `TokenVerifier` to not work correctly in Rails < 8.1.

After upgrading to the latest version of this gem, users would see
`ActionController::InvalidAuthenticityToken` error during the request
phrase, causing the OAuth authentication flow to fail.

The root cause of this issue was that the line
`include ActionController::RequestForgeryProtection` got moved below
the code that sets up the delegation to `ActionController::Base.config`.
This caused the delegation setup to silently fail as the list of
configurations to set up the delegation became empty.

Bug investigation: https://github.com/cookpad/omniauth-rails_csrf_protection/issues/26#issuecomment-3591652174

This issue was not caught by the test suite because the default
configuration values in `ActionController::RequestForgeryProtection`
and `ActionController::Base.config` were the same in the test suite.
However, in an actual Ruby on Rails application, Railtie would set
`per_form_csrf_tokens` to `true` on Action Controller during
application's initialization[1], causing the configuration to go
out-of-sync.

[1]: https://github.com/rails/rails/blob/v8.0.0/railties/lib/rails/application/configuration.rb#L114

This commit fixes the issue by moving `include` to the right location
for Rails 8.1+ and Rails < 8.1. To prevent further regression, this
commit also includes a new integration test that renders a mock sign-in
form to test the whole sign-in flow.

I'd like to thank @trysmr for a detailed investigation on the issue,
which helped me understand the root cause and also found the reason why
our test case didn't catch it.

Fixes #26.

lib/omniauth/rails_csrf_protection/token_verifier.rb
@@ -29,8 +29,15 @@ module OmniAuth
         def config
           self.class.config
         end
+
+        # For Rails 8.1+, includes this module after `config` is setup.
+        include ActionController::RequestForgeryProtection
       else
         include ActiveSupport::Configurable
+
+        # For Rails < 8.1, includes this module before delegation setup.
+        # Otherwise, `config` will be empty, and the delegation will fail.
+        include ActionController::RequestForgeryProtection
 
         # `ActionController::RequestForgeryProtection` contains a few
         # configurable options. As we want to make sure that our configuration is
@@ -43,9 +50,6 @@ module OmniAuth
           end
         end
       end
-
-      # Include this module only after we've prepared the configuration
-      include ActionController::RequestForgeryProtection
 
       def call(env)
         dup._call(env)

omniauth-rails_csrf_protection.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "omniauth", "~> 2.0"
 
   spec.add_development_dependency "bundler"
+  spec.add_development_dependency "capybara"
   spec.add_development_dependency "minitest"
 
   # We set requirement for Edge Rails in the Gemfile

test/application_test.rb
@@ -13,14 +13,14 @@ class ApplicationTest < Minitest::Test
     post "/auth/developer"
     follow_redirect!
 
-    assert last_response.not_found?
+    assert_equal "ActionController::InvalidAuthenticityToken", last_response.body
   end
 
   def test_request_phrase_with_bad_token_via_post
     post "/auth/developer", authenticity_token: "BAD_TOKEN"
     follow_redirect!
 
-    assert last_response.not_found?
+    assert_equal "ActionController::InvalidAuthenticityToken", last_response.body
   end
 
   def test_request_phrase_with_correct_token_via_post

test/integration_test.rb
@@ -1,0 +1,38 @@
+require "test_helper"
+require "capybara/rails"
+require "capybara/minitest"
+
+class IntegrationTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  # We are using this `:per_form_csrf_tokens` as a way to test that we have
+  # setup method delegation properly to prevent regression, as Railtie sets
+  # this configuration to true afterward and causes them to be out-of-sync.
+  setup do
+    @original_per_form_csrf_tokens = \
+      ActionController::Base.config[:per_form_csrf_tokens]
+    ActionController::Base.config[:per_form_csrf_tokens] = true
+  end
+
+  teardown do
+    ActionController::Base.config[:per_form_csrf_tokens] = \
+      @original_per_form_csrf_tokens
+
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+
+  def test_request_phrase
+    visit sign_in_path
+    click_on "Sign in"
+
+    refute page.has_content?("ActionController::InvalidAuthenticityToken")
+
+    fill_in "Name", with: "Kagari Mimi"
+    fill_in "Email", with: "mimi@example.com"
+    click_on "Sign In"
+
+    assert page.has_content?("Hello Kagari Mimi (mimi@example.com)!")
+  end
+end

test/test_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 # Simple Rails application template, based on Rails issue template
-# https://github.com/rails/rails/blob/master/guides/bug_report_templates/action_controller_gem.rb
+# https://github.com/rails/rails/blob/main/guides/bug_report_templates/action_controller.rb
 
 # Helper method to silence warnings from bundler/inline
 def silence_warnings
@@ -27,9 +27,11 @@ silence_warnings do
 
     if RUBY_VERSION >= "3.4"
       gem "bigdecimal"
+      gem "drb"
       gem "mutex_m"
     end
 
+    gem "capybara"
     gem "omniauth"
     gem "omniauth-rails_csrf_protection", path: File.expand_path("..", __dir__)
   end
@@ -73,7 +75,10 @@ class TestApp < Rails::Application
 
   # Define our custom routes. This needs to be called after initialize!
   routes.draw do
+    get "sign_in" => "application#sign_in"
     get "token" => "application#token"
+    get "auth/failure" => "application#failure"
+    match "auth/developer/callback" => "application#callback", :via => [:get, :post]
   end
 end
 
@@ -81,5 +86,19 @@ end
 class ApplicationController < ActionController::Base
   def token
     render plain: form_authenticity_token
+  end
+
+  def sign_in
+    render inline: <<~ERB
+      <%= button_to "Sign in", "/auth/developer", method: :post %>
+    ERB
+  end
+
+  def failure
+    render plain: params[:message]
+  end
+
+  def callback
+    render plain: "Hello #{params[:name]} (#{params[:email]})!"
   end
 end